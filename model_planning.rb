f253daeef10bee4f05ed2f17dcff26dde9196b52
Eric Clark <eclark0013@gmail.com>
2020-01-21 10:33:37 -0500

    plan out models and migrations for store and store_item classes

--- model_planning.rb
+++ model_planning.rb
@@ -9,3 +9,17 @@ Store
     Migration  
         t.string :name
         t.string :address
+    Model
+        validates :name, presence: true (possibly should add validation for address)
+        has_many :areas
+        has_many :store_items
+        has_many :items, through: :store_items
+        has_many :users, through: :user_stores
+Store_item
+    Migration
+        t.integer :store_id
+        t.integer :item_id
+    Model
+        belongs_to :store
+        belongs_to :item
+        #available? (defaults to true, open for extension)
