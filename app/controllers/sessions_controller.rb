e83d4e096ce0243eb99e886abfc19320313564bb
Eric Clark <eclark0013@gmail.com>
2020-01-22 12:20:45 -0500

    setting up sign in form with controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
     def new   
+        @user = User.new
     end
 
     def create
         if named_session?
-            session[:name] = params[:name]
-            redirect_to "/"
+            @user = User.find(name: params[:name])
+            if @user.authenticate(params[:password])
+                session[:name] = @user.name
+                raise @user.inspect
+                redirect_to "/"
+            end
         else
             redirect_to action: "new" 
         end 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+  root :to => "session#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
