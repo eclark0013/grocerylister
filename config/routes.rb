a0ade4f08dcc066cdebc771bcc00df9285399384
Eric Clark <eclark0013@gmail.com>
2020-01-22 11:49:17 -0500

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :items
   resources :stores
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
