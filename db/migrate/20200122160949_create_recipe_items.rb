2e760e419a9d931168e196b1c63e8e254d9b644f
Eric Clark <eclark0013@gmail.com>
2020-01-22 11:10:04 -0500

    generate recipe_item model

---
+++ db/migrate/20200122160949_create_recipe_items.rb
@@ -0,0 +1,12 @@
+class CreateRecipeItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipe_items do |t|
+      t.integer :recipe_id
+      t.string :item_id
+      t.string :integer
+      t.string :quantity
+
+      t.timestamps
+    end
+  end
+end
