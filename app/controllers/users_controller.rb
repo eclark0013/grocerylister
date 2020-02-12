04917c7063510192d252b9e478f1f81d270ab435
Eric Clark <eclark0013@gmail.com>
2020-02-12 12:03:34 -0500

    Fix bugs for new user signup that occur with bad data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,13 @@ class UsersController < ApplicationController
 
     def create #make a new user
         @user = User.create(user_params)
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error_messages] = @user.errors.full_messages
+            render "new"
+        end
     end
 
     private
